0dc56c9c27ef542e81da00ee471caccabae65f59
gabjetski <gabrielp2002@icloud.com>
2023-09-23 17:47:33 +0200

    finished frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import TestComponent from "./components/TestComponent.jsx"
 
 const App = () => {
-    return (
-        <div>
-          <TestComponent />
-        </div>
-    )
+  return (
+    <div>
+      <TestComponent />
+    </div>
+  )
 }
 
 export default App;
--- frontend/src/components/TestComponent.jsx
+++ frontend/src/components/TestComponent.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 
 const TestComponent = () => {
-    return (
-        <div>
-            Hi
-        </div>
-    );
+  return (
+    <div>
+      Hi
+    </div>
+  );
 }
 
 export default TestComponent;
